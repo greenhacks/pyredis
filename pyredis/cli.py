6d9a6d29d87325d482a7c6dbf43197b5fe5c55a9
Sam <stmiyamoto10@gmail.com>
2024-01-21 14:12:10 -0800

    Step 2 - skeleton for sending commands that a user enteres to the server, parsing the response, and rendering to the user

--- pyredis/cli.py
+++ pyredis/cli.py
@@ -9,7 +9,25 @@ def main(
     server: Annotated[str, typer.Argument()] = DEFAULT_SERVER,
     port: Annotated[str, typer.Argument()] = DEFAULT_PORT,
 ):
-    pass
+    
+    with socket.socket() as client_socket():
+        client_socket.connect((server, port))
+
+        buffer = bytearray()
+
+        while True:
+            command = input(f"{server}: {port>}")
+
+            if command == "quit":
+                break
+            
+            else:
+                encoded_message = encode_message(encode_command(command))
+                # TODO: send the serialized message to the server here
+
+                while True:
+                    # TODO: receive the response from the server, deserialize 
+                    # and display it here
 
 
 if __name__ == "__main__":
