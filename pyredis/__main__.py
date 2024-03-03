344c74edde33f5f4043ae1ecf14a73f7bb1950e6
Sam <stmiyamoto10@gmail.com>
2024-03-03 08:00:22 -0800

    Step 4 - async solution: handling concurrent clients with Async IO, commenting out regular server solution

--- pyredis/__main__.py
+++ pyredis/__main__.py
@@ -1,21 +1,42 @@
 import typer
+import asyncio
 
-from pyredis.server import Server
+from pyredis.asyncserver import RedisServerProtocol
+
+# from pyredis.server import Server
 
 REDIS_DEFAULT_PORT = 6379
 
 
-def main(port=None):
+async def main(port=None):
     if port == None:
         port = REDIS_DEFAULT_PORT
     else:
         port = int(port)
-
     print(f"Starting PyRedis on port: {port}")
 
-    server = Server(port)
-    server.run()
+    loop = asyncio.get_running_loop()
+
+    server = await loop.create_server(
+        lambda: RedisServerProtocol(), "127.0.0.1", REDIS_DEFAULT_PORT
+    )
+
+    async with server:
+        await server.serve_forever()
+
+
+# def main(port=None):
+#     if port == None:
+#         port = REDIS_DEFAULT_PORT
+#     else:
+#         port = int(port)
+
+#     print(f"Starting PyRedis on port: {port}")
+
+#     server = Server(port)
+#     server.run()
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    # typer.run(main)
+    typer.run(asyncio.run(main()))
